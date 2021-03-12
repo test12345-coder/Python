1c44b1635cbdbf9a4102de92912d23bec80a0d1c
Saadettin Atay <45216743+S4d0@users.noreply.github.com>
2021-03-13 00:31:28 +0300

    Update mukemmelSayi.py

--- mukemmelSayi.py
+++ mukemmelSayi.py
@@ -1,1 +1,19 @@
-# -*- coding: utf-8 -*-# =============================================================================# Mükemmel sayı, sayılar teorisinde, kendisi hariç pozitif tam bölenlerinin toplamı # kendisine eşit olan sayı. Diğer bir ifadeyle, bir mükemmel sayı, bütün pozitif tam # bölenlerinin toplamının yarısına eşittir.# =============================================================================def perfectNum(number):    count= 0    for i in range(number-1,  0, -1):        if number % i == 0:            count += i        if i == 1 and count == number:            print("True")        elif i == 1:            print("False")if __name__ == '__main__':    perfectNum(6) # True    perfectNum(3) # False
+'''
+Mükemmel sayı, sayılar teorisinde, kendisi hariç pozitif tam bölenlerinin toplamı 
+kendisine eşit olan sayı. Diğer bir ifadeyle, bir mükemmel sayı, bütün pozitif tam 
+bölenlerinin toplamının yarısına eşittir.
+'''
+
+def perfectNum(number):
+    count= 0
+    for i in range(number-1,  0, -1):
+        if number % i == 0:
+            count += i
+        if i == 1 and count == number:
+            print("True")
+        elif i == 1:
+            print("False")
+
+if __name__ == '__main__':
+    perfectNum(6) # True
+    perfectNum(3) # False
