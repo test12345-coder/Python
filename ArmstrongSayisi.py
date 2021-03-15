e8accd792b530f61f950bf50e09f23e7087ae6f2
Saadettin Atay <45216743+S4d0@users.noreply.github.com>
2021-03-15 23:49:35 +0300

    Update ArmstrongSayisi.py

--- ArmstrongSayisi.py
+++ ArmstrongSayisi.py
@@ -1,1 +1,15 @@
-def armstrong(number):    sayı = str(number)    power = len(sayı)    toplam = 0    for eleman in sayı:        toplam += int(eleman) ** power    if toplam == number:        print(number ,"bu sayı Armstrong sayısıdır")    else:        print(number, "bu sayı Armstrong sayısı değildir")     if __name__ == '__main__':    armstrong(8208) # True    armstrong(1234) # False    
+def armstrong(number):
+  sayı = str(number)
+  power = len(sayı)
+  toplam = 0
+  for eleman in sayı:
+    toplam += int(eleman) ** power
+  if toplam == number:
+    print(number ,"bu sayı Armstrong sayısıdır")
+  else:
+    print(number, "bu sayı Armstrong sayısı değildir")
+
+
+if __name__ == '__main__':
+  armstrong(8208) # True
+  armstrong(1234) # False
