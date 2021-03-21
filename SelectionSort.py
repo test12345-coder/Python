b787a822fbfa331f6b9a26fd55b77874f24fafc4
Saadettin Atay <45216743+S4d0@users.noreply.github.com>
2021-03-22 01:11:23 +0300

    Update SelectionSort.py

--- SelectionSort.py
+++ SelectionSort.py
@@ -1,1 +1,13 @@
-def SelectionSort(liste):    for i in range(len(liste) - 1):        minimum = i         for j in range(i +  1, len(liste) - 1):            if(liste[j] < liste[minimum]):                minimum = j        if(minimum != i):            liste[i], liste[minimum] = liste[minimum], liste[i]    return liste          if __name__ == '__main__':    liste = [12, 40, 8, 33, 52, 3, 10, 83]    print(SelectionSort(liste))
+def SelectionSort(liste):
+  for i in range(len(liste) - 1):
+    minimum = i 
+    for j in range(i +  1, len(liste) - 1):
+      if(liste[j] < liste[minimum]):
+        minimum = j
+    if(minimum != i):
+      liste[i], liste[minimum] = liste[minimum], liste[i]
+    return liste
+          
+if __name__ == '__main__':
+  liste = [12, 40, 8, 33, 52, 3, 10, 83]
+  print(SelectionSort(liste))
